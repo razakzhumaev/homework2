7a719faf1de8330549eebfe6ea6a2ce51420bf19
zhumaevrazak <zhumaevrazak@gmail.com>
2023-10-09 18:15:51 +0600

    first commit

---
+++ practice3.dart
@@ -0,0 +1,107 @@
+void main(List<String> args) {
+  Map userInfo = {
+    'name': 'razak',
+    'surname': 'zhumaev',
+    'age': 18,
+    'education': 'computer science'
+  };
+  print(userInfo);
+  Map userInfo1 = {'height': '180cm', 'weight': '85kg'};
+  print(userInfo1);
+  Map userInfo2 = {
+    'monday': 'chemistry',
+    'tuesday': 'biology',
+    'wednesday': 'math',
+    'thursday': 'english',
+    'friday': 'physics'
+  };
+  print(userInfo2);
+  Map userInfo3 = {
+    'summer': 'go to the lake',
+    'autumn': 'chilling',
+    'winter': 'go to skiing'
+  };
+  print(userInfo3);
+
+  var test = 'appple';
+  test = 'melon';
+  print(test);
+
+  var test1;
+  test1 = 'watermelon';
+  test1 = 100000;
+  test1 = 23.3;
+  test1 = ['hi', 'i am 18 y.o', 232323, 23232.32323];
+  print(test1);
+
+  dynamic test2 = 12;
+test2 = 323.3232;
+test2 = 'razak';
+test2 = [21, 121.121, 'razak','razak 18 y.o'];
+test2 = {'name':'razak', 'surname':'zhumaev'};
+print(test2);
+
+dynamic test3 = 12;
+print(test3);
+test3 = 323.3232;
+print(test3);
+test3 = 'razak';
+print(test3);
+test3 = [21, 121.121, 'razak','razak 18 y.o'];
+print(test3);
+test3 = {'name':'razak', 'surname':'zhumaev'};
+print(test3);
+
+final userInf = 'razak';
+print(userInf);
+final userInf1;
+userInf1 = 'razak';
+print(userInf1);
+final userInf2 = true;
+print(userInf2);
+final userInf3;
+userInf3 = true;
+print(userInf3);
+final userInf4 = 111;
+print(userInf4);
+final userInf5;
+userInf5 = 111;
+print(userInf4);
+
+const smf = false;
+print(smf);
+const smf1 = 'razak';
+print(smf1);
+const smf2 = 'razak 18 years old';
+print(smf2);
+const smf3 = [232, 434.343, 'where are you?', true, false];
+print(smf3);
+
+
+
+
+  int a = 2;
+  print(a + a);
+  int b = 5;
+  int c = 10;
+  print(b - c);
+  int d = 16;
+  int e = 3;
+  print(d * e);
+  int f = 18;
+  int g = 2;
+  print(f ~/ g);
+
+  double a1 = 10.5;
+  double b1 = 2.5;
+  print(a1 / b1);
+
+  int h = 12;
+  int i = 15;
+  print(h > i);
+  print(h < i);
+  print(h <= i);
+  print(h >= i);
+  print(h == i);
+  print(h != i);
+}
